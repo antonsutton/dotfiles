df75be718121617a164f0f741be5386e18163c93
Anton Sutton <anton@project458.com>
2012-03-20 22:25:59 +0200

    some prompt tweaks dude. sweet.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -68,22 +68,52 @@ todo(){
   fi
 }
 
-directory_name(){
+# Returns the type of session we're in: empty string for normal terminal locally or [rem/ssh] for remote sessions
+function sesh_type { 
+  local sesh=""
+  
+  if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
+      sesh="[rem/ssh] "
+    # many other tests omitted
+    else
+      case $(ps -o comm= -p $PPID) in
+        sshd|*/sshd) sesh="[rem/ssh] ";;
+      esac
+  fi
+  echo "$sesh"
+}
+
+# Return either hostname -s (shortened) or the contetns of the file .box-name if it exists
+function box_name { 
+  [ -f ~/.box-name ] && cat ~/.box-name || hostname -s
+}
+
+function directory_name(){
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
 if [ $UID -eq 0 ]; then NCOLOR="red"; else NCOLOR="white"; fi
 
-export PROMPT='%{$fg[$NCOLOR]%}%B%n%b%{$reset_color%}:%{$fg[blue]%}%B${PWD/#$HOME/~}/%b%{$reset_color%} $(git_dirty)$(need_push)%(!.#.) '
+# %(?..) means 'True if the exit status of the last command was n. n = 0 when left out'
+# So this line will be a red x:exit code if the last command return a non-zero exit code
+local return_status="%(?..•%{$fg[red]%}✘%{$reset_color%}:)%(?,,%{${fg_bold[white]}%}%?%{$reset_color%})"
+
+# will look like this:
+# antonsutton@scruffy:~/.dotfiles/ on master                                           •22:20:33•Just do it:4•
+export PROMPT='$(sesh_type)%{$fg[$NCOLOR]%}%B%n%b%{$reset_color%}@%{$fg[yellow]%}$(box_name)%{$reset_color%}:%{$fg[blue]%}%B${PWD/#$HOME/~}/%b%{$reset_color%} $(git_dirty)$(need_push)%(!.#.) '
 
 #export PROMPT=$'\n%m at $(rvm_prompt) in $(collapse_pwd) $(git_dirty)$(need_push)\n› '
 #export PROMPT=$'\n$(rvm_prompt) in $(collapse_pwd) $(git_dirty)$(need_push)\n› '
 
-set_prompt () {
-  export RPROMPT='%{$fg_bold[magenta]%}[%*]%{$reset_color%} Just do it: %{$fg_bold[white]%}$(todo)%{$reset_color%}'
+function set_prompt () {
+  export RPROMPT='${return_status}%{$reset_color%}•%{$fg_bold[magenta]%}%*%{$reset_color%}•Just do it:%{$fg_bold[white]%}$(todo)%{$reset_color%}•'
 }
 
-precmd() {
-  title "zsh" "%m" "DIE %55<...<%~"
+function precmd() {
+  title "zsh" "$USER@%m" "%55<...<%~"
   set_prompt
+}
+
+function preexec() {
+  title "$1" "$USER@%m" "%35<...<%~"
 }
